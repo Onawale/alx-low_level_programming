95d4a7eaf870298ed93ef63b0e62a741ad0385d4
Onawale <onatoyinboonawale@gmail.com>
2023-10-09 19:57:57 -0700

    and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - this print exactly the input on it
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

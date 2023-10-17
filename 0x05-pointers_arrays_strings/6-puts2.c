a83b7eada2b0119f5bc10494d669b490886866af
Onawale <onatoyinboonawale@gmail.com>
2023-10-17 04:24:08 -0700

    Error Check

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0 ; str[i] != '\0' ; i++){
-		if (1 % 2 == 0){
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (1 % 2 == 0)
 			_putchar(str[i]);
 
 	}
-}
 		_putchar('\n');
 }

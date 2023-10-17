b0a96ef8b3c9ff3f93551c7848a7696c7eb06f9e
Onawale <onatoyinboonawale@gmail.com>
2023-10-17 04:44:24 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
- # include "main.h"
+#include "main.h"
 
 /**
  * puts2 - print every other character of a string
@@ -12,9 +12,8 @@ void puts2(char *str)
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (1 % 2 == 0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
-
 	}
-		_putchar('\n');
+	_putchar('\n');
 }

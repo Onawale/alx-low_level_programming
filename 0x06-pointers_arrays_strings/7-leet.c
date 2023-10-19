f351312e052bb9c8bdc09c0682ccd49b79201314
Onawale <onatoyinboonawale@gmail.com>
2023-10-19 05:32:11 -0700

    Letters for numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,14 @@ char *leet(char *n)
 	char s1[] = "aAeEoOtTIL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0' ; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if  (n[i] == s1[j])
 			{
 				n[i] = s2[j];
+				break;
 			}
 		}
 

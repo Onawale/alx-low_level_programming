4bf8698ae5782643bb5f09552a6b9679d674ffc8
Onawale <onatoyinboonawale@gmail.com>
2023-10-19 07:26:31 -0700

    1337 encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * leet - encode into 1337
- *
- * @n: input value
- *
- * Return: n value
+ * leet - encode into 1337speak
+ * @n: input string
+ * Return: n (the modified string)
  */
 
 char *leet(char *n)
 {
-	int i, j;
+	 int i, j;
+
 	char s1[] = "aAeEoOtTIL";
 	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-	for (j = 0; j < 10; j++)
-	{
-	if  (n[i] == s1[j])
-	{
-	n[i] = s2[j];
-	}
-	}
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+				break;
+			}
+		}
 	}
 	return (n);
 }

01d5f6813b8d444cce95fe7adc7ecea0ad571f0b
Onawale <onatoyinboonawale@gmail.com>
2023-10-19 08:05:14 -0700

    1337 encoding

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -10,8 +12,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif

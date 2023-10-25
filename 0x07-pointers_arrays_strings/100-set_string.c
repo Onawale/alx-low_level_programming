b665908c21cc5b92a8a7b729165ab39ca5e4d526
Onawale <onatoyinboonawale@gmail.com>
2023-10-24 21:56:49 -0700

    100 Set String

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,3 +1,17 @@
 #include "main.h"
+#include "main.h"
 
+/**
+  * set_string - main function
+  *
+  * @s: Function parameter
+  *
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
 
+{
+	*s = to;
+}

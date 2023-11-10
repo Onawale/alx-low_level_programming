ab2a4ff352ae5e5d6c75c9b14e7a480982e10666
Onawale <onatoyinboonawale@gmail.com>
2023-11-09 18:37:09 -0800

    sum of two number

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "variadic_function.h"
+#include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
@@ -12,7 +12,8 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int 1, sum = 0;
+
+	unsigned int i, sum = 0;
 
 	va_start(ap, n);
 

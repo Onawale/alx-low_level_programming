5164be6ac33acf6625f6b959657459e2ddc0c64c
Onawale <onatoyinboonawale@gmail.com>
2023-11-16 20:39:10 -0800

    Optimise 0-print_list code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_list - prints all the elememts of a list_t list
@@ -10,14 +12,17 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+
 		nodes++;
+		h = h->next;
 	}
+
 	return (nodes);
 }
